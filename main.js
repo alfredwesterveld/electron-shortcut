314befa37ace1ef939e5847ecd57af3d1ef1e451
Alfred Westerveld <alfredwesterveld@gmail.com>
2016-08-22 10:56:38 +0200

    Also be able to spawn process

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 'use strict';
 
-const {app, BrowserWindow, globalShortcut} = require('electron')
-const _ = require('lodash')
+const {app, BrowserWindow, globalShortcut} = require('electron');
+const _ = require('lodash');
 const argv = require('minimist')(process.argv.slice(2));
-const request = require('request')
+const request = require('request');
+const execFile = require('child_process').execFile;
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win, config;
+let win;
 
-try {
-    config = require(`./${argv.config}`)
-} catch (e) {
-    console.log(`Could not locate config file ${e}`);
-    process.exit(1);
-}
+const config = (function loadConfig() {
+    try {
+        const _config = argv.config ? argv.config : './config.json';
+        return require(_config);
+    } catch (e) {
+        console.log(`Could not locate config file ${e}`);
+        process.exit(1);
+    }
+}());
+
 
 function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({
-        show: false,
+        show: argv.show || false,
         width: 800,
         height: 600
     });
@@ -28,7 +34,7 @@ function createWindow () {
     win.loadURL(`file://${__dirname}/index.html`);
 
     // Open the DevTools.
-    //win.webContents.openDevTools();
+    win.webContents.openDevTools();
 
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -46,14 +52,25 @@ function registerShortcuts () {
         console.log(JSON.stringify([key, value]));
 
         const ret = globalShortcut.register(key, () => {
-            request.get(value, (error, response, body) => {
-                if (error) {
-                    console.log(`request ${key} had error`);
-                } else {
-                    console.log(JSON.stringify([key, body]));
-                    contents.send('request', body);
-                }
-            });
+            if (value.startsWith('http')) {
+                request.get(value, (error, response, body) => {
+                    if (error) {
+                        console.log(`request ${key} had error ${error}`);
+                    } else {
+                        console.log(JSON.stringify([key, body]));
+                        contents.send('request', body);
+                    }
+                });
+            } else {
+                execFile(value, (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(`request ${key} had error ${error}`);
+                    }
+                    console.log(JSON.stringify([key, stdout]));
+                    console.log(JSON.stringify([key, stderr]));
+                    contents.send('request', stdout);
+                });
+            }
         });
 
         if (!ret) {
@@ -62,15 +79,14 @@ function registerShortcuts () {
     });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', () => {
+function onReady() {
     createWindow();
     registerShortcuts();
-});
+}
 
-app.on('will-quit', () => {
-    // Unregister all shortcuts.
+function onWillQuit() {
     globalShortcut.unregisterAll();
-});
+}
+
+app.on('ready', onReady);
+app.on('will-quit', onWillQuit);
